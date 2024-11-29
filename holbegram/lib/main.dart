1b44f2c695644468b2bbb4bbd9d135e403e9436d
HolbyKate <6804@holbertonstudents.com>
2024-11-29 16:09:40 +0100

    Fix main dart

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'screens/login_screen.dart';
+import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Holbegram',
+      title: 'com.example.holbegram',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
